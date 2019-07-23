537432548a3a155bfd2714a194d612f98540256a
Israel Ekpo <37992436+izzymsft@users.noreply.github.com>
2019-07-23 11:05:28 -0400

    Adding DDLs

---
+++ Project.DDL.sql
@@ -0,0 +1,27 @@
+IF OBJECT_ID('dbo.PatientRecordsSource', 'U') IS NOT NULL 
+  DROP TABLE dbo.PatientRecordsSource; 
+
+CREATE TABLE dbo.PatientRecordsSource --- Table for Storing Patient Scores
+(
+    PatientId int IDENTITY (1,1) NOT NULL,      --- Primary Key for the Score
+    FirstName VARCHAR(40) NOT NULL,             --- First Name of the Patient
+    LastName VARCHAR(40) NOT NULL,              --- Last Name of the Patient
+    WeightPounds DECIMAL(5,2) NOT NULL,         --- The Last Weight of the Patient
+    heightInches DECIMAL(5,2) NOT NULL,         --- The Last Height of the Patient
+    DateOfBirth date NOT NULL,                  --- The Patients DOB
+    DateCreated datetime2 NOT NULL              --- The timestamp for the record creation
+);
+
+IF OBJECT_ID('dbo.PatientRecordsDestination', 'U') IS NOT NULL 
+  DROP TABLE dbo.PatientRecordsDestination; 
+
+CREATE TABLE dbo.PatientRecordsDestination --- Table for Storing Patient Scores
+(
+    PatientId int IDENTITY (1,1) NOT NULL,      --- Primary Key for the Score
+    FirstName VARCHAR(40) NOT NULL,             --- First Name of the Patient
+    LastName VARCHAR(40) NOT NULL,              --- Last Name of the Patient
+    WeightKilograms DECIMAL(5,2) NOT NULL,      --- The Last Weight of the Patient
+    heightCentimeters DECIMAL(5,2) NOT NULL,    --- The Last Height of the Patient
+    DateOfBirth date NOT NULL,                  --- The Patients DOB
+    DateCreated datetime2 NOT NULL              --- The timestamp for the record creation
+);
